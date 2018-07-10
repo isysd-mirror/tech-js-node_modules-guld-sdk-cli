2d8755801131fdfd1c6121dec0429c83b6e0135b
Ira Miller <public@iramiller.com>
2018-07-10 13:17:09 -0500

    init

---
+++ cli.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+const program = require('commander')
+const path = require('path')
+const { getRandStr, getRandInt } = require('guld-random')
+const VERSION = require('./package.json').version
+
+/* eslint-disable no-console */
+program
+  .name('guld-random')
+  .version(VERSION)
+  .description("Cryptographically secure random number generator using `/dev/urandom` with fallback to node's `crypto` and finally to `window.crypto || window.mscrypto`.")
+  .command('string [length]')
+  .alias('str')
+  .description('Generate a random string of the given length (default 256).')
+  .action((length, options) => {
+    getRandStr(length).then(console.log)
+  })
+program
+  .command('number [max]')
+  .alias('num')
+  .description('Generate a random integer between 0 and max. (default 65536)')
+  .action(async (maxm, options) => {
+    getRandInt(maxm).then(console.log)
+  })
+/* eslint-enable no-console */
+
+program.parse(process.argv)
